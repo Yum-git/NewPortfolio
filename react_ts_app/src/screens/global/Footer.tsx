f1c7026e596102663c662010def906d6e4a3cc01
Yum-git <yzk.yuma@gmail.com>
2021-01-21 06:51:39 +0900

    Add フッターの共通部分を作成しました

---
+++ react_ts_app/src/screens/global/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="main_footer">
+                <label>©️2021 yuma ohta</label>
+            </div>
+        );
+    }
+}
+
+export default Footer;
